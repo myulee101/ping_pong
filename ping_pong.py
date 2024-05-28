4d040731a77c879245ac4fc0c0e118d1619f95ab
myulee101 <ersultan.r2010@gmail.com>
2024-05-28 21:51:57 +0500

    Update ping_pong.py
    v.1.1 добавлен мяч

--- ping_pong.py
+++ ping_pong.py
@@ -1,4 +1,23 @@
 from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+
+
+
 #игровая сцена
 win_color = (254, 239, 239) #цвет фона
 win_width = 600
@@ -10,6 +29,7 @@ clock = time.Clock()
 fps = 60
 game = True
 
+ball = GameSprite('ping_pong_image.png', 200, 200, 4, 50, 50)
 #игровой цикл
 
 while game:
@@ -17,5 +37,6 @@ while game:
         if e.type == QUIT:
             game = False
 
+    ball.reset()
     display.update()
     clock.tick(fps)        
